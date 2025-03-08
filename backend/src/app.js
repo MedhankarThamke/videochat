9c31ee3e18ba70f55e34ee323cfcb4905f8fc923
MedhankarThamke <tmedhankar@gmail.com>
2025-03-08 15:16:31 +0530

    route problem solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users", userRoutes);
+app.use("api/v1/users", userRoutes);
 
 const start = async () => {
   const connectionDb = await mongoose.connect(process.env.MONGO_URI, {
